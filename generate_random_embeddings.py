5c508bf787984aa8e4c0954a94f0ae5a3ab9d687
talkana <nataliaruteckaa@gmail.com>
2023-06-29 00:43:17 +0200

    fix basic random vectors generator

--- generate_random_embeddings.py
+++ generate_random_embeddings.py
@@ -5,8 +5,8 @@ def generate_random_vectors(k, d, file_path, min_val, max_val):
     np.save(file_path, vectors)
 
 # Usage example
-k = 5  # Number of vectors
-d = 3  # Dimension of vectors
+k = 10  # Number of vectors
+d = 30  # Dimension of vectors
 file_path = "vectors.npy"  # File path to save the vectors
 min_val = -100  # Minimum value for the range
 max_val = 100  # Maximum value for the range
